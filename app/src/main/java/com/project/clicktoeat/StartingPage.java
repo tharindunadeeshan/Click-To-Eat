15b69eb1245e89c9a004658fb70c0db8f712ed91
tharindunadeeshan <57563360+tharindunadeeshan@users.noreply.github.com>
2021-03-21 22:00:42 -0700

    Initial commit

---
+++ app/src/main/java/com/project/clicktoeat/StartingPage.java
@@ -0,0 +1,56 @@
+package com.project.clicktoeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class StartingPage extends AppCompatActivity {
+    private  static  int SPLASH_SCREEN =2000;
+
+    //variables
+    Animation topAnim,bottomAnim;
+    ImageView iamge;
+    TextView logo,slogan;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_starting_page);
+
+        //Animations
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //Hooks
+        iamge =findViewById(R.id.imgLogoSP);
+        logo =findViewById(R.id.txtAppNameSP);
+        slogan =findViewById(R.id.txtDescSP);
+
+        iamge.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+       new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(StartingPage.this,AddLocation.class );
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
+    }
+}
